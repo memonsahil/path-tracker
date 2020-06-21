308f1636181189b5d566797f42df6ec7b7d19622
Sahil Memon <sahilmemon@pc-133-11.customer.ask4.lan>
2020-06-21 14:37:22 +0100

    commit #3

---
+++ src/components/Spacer.js
@@ -0,0 +1,20 @@
+//For general spacing and applying margin to any child components.
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Spacer = ({ children }) => {
+    return (
+        <View style = {styles.spacer}>
+            { children }
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    spacer: {
+        margin: 15
+    }
+});
+
+export default Spacer;
---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const TrackListScreen = ({ navigation }) => {
+    return (
+        <>
+            <Text style = {{ fontSize: 48 }}>
+                TrackListScreen
+            </Text>
+            <Button
+                title = 'Go to TrackDetail'
+                onPress = {() => navigation.navigate('TrackDetail')}
+
+            />
+        </>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
