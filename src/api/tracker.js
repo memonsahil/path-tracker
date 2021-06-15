63f32daa3fef808cea6102a56a2a69ca3e7991ba
Sahil Memon <sahilm1899@gmail.com>
2021-06-15 23:27:03 +0100

    Update tracker.js

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-community/async-storage";
 
-//Using ngrok, hence the baseURL changes every 8 hours.
 const instance = axios.create({
   baseURL: "http://3bb5ff501a80.ngrok.io",
 });
 
-//Function to intercept every request instance.
+// Method to intercept every request instance.
 instance.interceptors.request.use(
   async (config) => {
-    //Handling configuration by passing the jwt with every request instance.
     const token = await AsyncStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -17,7 +15,6 @@ instance.interceptors.request.use(
     return config;
   },
   (err) => {
-    //Handling errors is found any.
     return Promise.reject(err);
   }
 );
