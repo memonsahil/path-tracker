ad9785138420d9d13205df8b467aed8907feba43
Sahil Memon <sahilm1899@gmail.com>
2021-06-16 23:43:27 +0100

    Update ResolveAuthScreen.js

--- src/screens/ResolveAuthScreen.js
+++ src/screens/ResolveAuthScreen.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Context as AuthContext } from "../context/AuthContext";
 
-//For resolving a user's authentication state by confirming the presence of a token (jwt).
 const ResolveAuthScreen = () => {
   const { tryLocalSignin } = useContext(AuthContext);
 
