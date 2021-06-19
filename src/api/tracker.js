8d07a33e40fde2905d7cf1816bc3ac05690932aa
Sahil Memon <sahilm1899@gmail.com>
2021-06-19 01:35:06 +0100

    Update tracker.js

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-community/async-storage";
 
+/*
+Connect to the mongoDB database from the location-tracking-server project. ngrok is
+setup in location-tracking-server and is used to redirect the baseURL to the database.
+Replace the current baseURL with the ngrok forwareded URL. Note that the ngrok forwarded URL
+changes every 2 hours; therefore, it needs to be updated accordingly.
+*/
+
 const instance = axios.create({
-  baseURL: "https://2eac0d12accc.ngrok.io",
+  baseURL: "https://b2c5d295eac4.ngrok.io",
 });
 
 // Method to intercept every request instance.
